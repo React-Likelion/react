b207e550fee5bcbab8b46b537a1856c9a41d2c7b
forever2969 <forever296@naver.com>
2022-08-04 00:18:29 +0900

    회원가입 페이지 완

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,12 +1,55 @@
-import React from 'react';
-import '../style/pages/SignupPage.css';
+import React,{useState} from 'react';
+import '../style/SignUpForm.css';
 
-const SignupPage = () => {
+const SignUpPage = () => {
+    const [signUpData,setSignUpData] = useState({
+        id:'',
+        password:'',
+        name:'',
+        phone:'',
+        email:'',
+        birth:'',
+        job:'',
+    });
+
+    const handleChangeData = (e) => {
+        setSignUpData({
+            ...signUpData,
+            [e.target.name]: e.target.value,
+        });
+    };
+    const clickSignUp = (e)=>{
+        e.preventDefault();
+        //회원가입 통신
+    };
+
     return (
-        <div>
-            
+        <div id="signupDiv">
+            <section id="signupLeft">
+                <h1>Re:act</h1>
+            </section>
+            <section id="signupRight">
+                
+                <form id="signupForm" onSubmit={clickSignUp}>
+                    <h2>회원가입</h2>
+                    <input type="text" onChange={handleChangeData} name='id' placeholder='아이디를 입력하세요' /><br />
+                    <input type="text" onChange={handleChangeData} name='password' placeholder='비밀번호를 입력하세요' /><br />
+                    <input type="text" onChange={handleChangeData} name='confirmPassword' placeholder='비밀번호를 한번 더 입력하세요' /><br />
+                    <input type="text" onChange={handleChangeData} name='name' placeholder='이름을 입력하세요' /><br />
+                    <input type="text" onChange={handleChangeData} name='phone' placeholder='전화번호를 입력하세요' /><br />
+                    <div>
+                        <input type="text" onChange={handleChangeData} name='email' id="emailInput" placeholder='이메일' />@
+                        <input id="emailAdressInput" type="text" />
+                        <button type="button">인증코드 받기</button>
+                    </div>
+                    <input type="text" name='confirmNum' placeholder='인증번호 입력' />
+                    <input type="text" onChange={handleChangeData} name='birth' placeholder='생년월일을 입력하세요(8자)' /><br />
+                    <input type="text" onChange={handleChangeData} name='job' placeholder='직업을 입력하세요' /><br />
+                    <input type="submit" id="signupSubmit" value="가입하기" />
+                </form>
+            </section>
         </div>
     );
 };
 
-export default SignupPage;
+export default SignUpPage;
