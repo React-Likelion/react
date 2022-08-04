e46e51eb353b02a4eaf1aa8b07a227c895665255
freemoon99 <soloist132@gmail.com>
2022-08-05 00:20:03 +0900

    멘토링페이지, css

--- src/pages/Mentoring/MentoringPage.js
+++ src/pages/Mentoring/MentoringPage.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import Header from './../../components/Header';
+import '../../style/pages/Mentoring/MentoringPage.css'
+import Navbar from '../../components/Navbar';
+import MentoringBox from './../../components/MentoringPage/MentoringBox';
 
 const MentoringPage = () => {
+    const optionList = [
+        "인기순", "최신순"
+    ]
+
+    const optionHandler = (e) => {
+
+    }
     return (
         <div>
-            
+            <Header />
+            <Navbar />
+            <div style={{border:'2px solid red'}}>
+                필터링 박스
+            </div>
+            <div className='selectBar'>
+                <select onChange={optionHandler} /*value={option}*/>
+                        <option disabled>정렬</option>
+                        {
+                            optionList.map((op,i) => (
+                                <option value={op} key={i}>
+                                    {op}
+                                </option>
+                            ))
+                        }
+                </select>
+            </div>
+            <section className='viewSection'>
+                <MentoringBox />
+            </section>
         </div>
     );
 };
