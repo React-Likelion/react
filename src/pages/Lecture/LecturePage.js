0309254a2e831b8e754dc95c6b06e55b4ae3aea0
forever2969 <forever296@naver.com>
2022-08-05 15:19:52 +0900

    강의페이지 카테고리 70% 완성

--- src/pages/Lecture/LecturePage.js
+++ src/pages/Lecture/LecturePage.js
@@ -1,9 +1,22 @@
 import React from 'react';
+import Header from '../../components/Header';
+import Navbar from '../../components/Navbar';
+import LectureCategory from '../../components/LecturePage/LectureCategory';
+import '../../style/pages/Lecture/LecturePage.css';
 
 const LecturePage = () => {
     return (
-        <div>
-            
+        <div id="lecturePageDiv">
+            <Header />
+            <Navbar />
+            <div id="lectureContainerBox">
+                <section id="lectureLeftContainer">
+                    <LectureCategory />
+                </section>
+                <section id="lectureRightContainer">
+
+                </section>
+            </div>
         </div>
     );
 };
