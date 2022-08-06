5896212efe2e8d817ca4321275bc6dc55b007e04
JaeHyunGround <gothddlek@naver.com>
2022-08-07 02:27:31 +0900

    Filter change  color yet

--- src/pages/Club/ClubPage.js
+++ src/pages/Club/ClubPage.js
@@ -1,10 +1,21 @@
 import React from 'react';
+import "../../style/pages/Club/ClubPage.css"
 
 const ClubPage = () => {
+    const dummyData = {
+            id : 1,
+            tag : [사업, 기획, 창업],
+            title : "사업계획서 기획에서 창업 지원사업까지",
+            content : "예비 창업자, 창업에 관심있는 분들 ~",
+            writer : "모네",
+            members : 5,
+            img : "/img/lectureBtn"
+        }
+
     return (
-        <div>
+        <section>
             
-        </div>
+        </section>
     );
 };
 
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../style/pages/MainPage.css';
-import Header from './../components/Header';
-import Navbar from './../components/Navbar';
+import Header from "../components/Header";
+import Navbar from '../components/Navbar';
 
 const MainPage = () => {
     return (
