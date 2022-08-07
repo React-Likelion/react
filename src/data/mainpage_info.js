fabde0ac3acc964fcb36744faa4c963c91604c3e
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-08-07 21:38:09 +0900

    add dummy datas

---
+++ src/data/mainpage_info.js
@@ -0,0 +1,191 @@
+const lecture_data = [
+    {
+        'id' : 1,
+        'title' : '프로그래밍 입문자를 위한 베테랑의 팁!',
+        'description' : '입문자를 위한 HTML 기초',
+        'price' : 150000,
+        'image' : 'image/1.png',
+        'youtube_link' : 'https://www.youtube.com/watch?v=sB5XLKqFd3g&ab',
+        'field' : 'IT 프로그래밍',
+        'tag' : 'WEB',
+        'like_member' : [
+            '123123', 
+            '124124'
+        ],
+        'like_cnt' : 2,
+        'visit_cnt' : 10,
+        'create_time' : '2022-08-03T22:17:23.559772+09:00',
+    }, 
+    {
+        'id' : 2,
+        'title' : '프로그래밍 입문자를 위한 베테랑의 팁!',
+        'description' : '입문자를 위한 HTML 기초',
+        'price' : 150000,
+        'image' : 'image/1.png',
+        'youtube_link' : 'https://www.youtube.com/watch?v=sB5XLKqFd3g&ab',
+        'field' : 'IT 프로그래밍',
+        'tag' : 'WEB',
+        'like_member' : [
+            '123123', 
+            '124124'
+        ],
+        'like_cnt' : 2,
+        'visit_cnt' : 10,
+        'create_time' : '2022-08-03T22:17:23.559772+09:00',
+    }, 
+    {
+        'id' : 3,
+        'title' : '프로그래밍 입문자를 위한 베테랑의 팁!',
+        'description' : '입문자를 위한 HTML 기초',
+        'price' : 150000,
+        'image' : 'image/1.png',
+        'youtube_link' : 'https://www.youtube.com/watch?v=sB5XLKqFd3g&ab',
+        'field' : 'IT 프로그래밍',
+        'tag' : 'WEB',
+        'like_member' : [
+            '123123', 
+            '124124'
+        ],
+        'like_cnt' : 2,
+        'visit_cnt' : 10,
+        'create_time' : '2022-08-03T22:17:23.559772+09:00',
+    }, 
+    {
+        'id' : 4,
+        'title' : '프로그래밍 입문자를 위한 베테랑의 팁!',
+        'description' : '입문자를 위한 HTML 기초',
+        'price' : 150000,
+        'image' : 'image/1.png',
+        'youtube_link' : 'https://www.youtube.com/watch?v=sB5XLKqFd3g&ab',
+        'field' : 'IT 프로그래밍',
+        'tag' : 'WEB',
+        'like_member' : [
+            '123123', 
+            '124124'
+        ],
+        'like_cnt' : 2,
+        'visit_cnt' : 10,
+        'create_time' : '2022-08-03T22:17:23.559772+09:00',
+    }
+];
+
+
+const mentoring_data = [
+    {
+        "id": 1,
+        "location": "지역1",
+        "field": "분야1",
+        "title": "제목1",
+        "description": "내용1",
+        "age_group": 5,
+        "limit": 5,
+        "tag": ["태그1", "태그2", "태그3"],
+        "image": "이미지1",
+        "member": null,
+        "member_cnt": 5
+    }, 
+    {
+        "id": 2,
+        "location": "지역1",
+        "field": "분야1",
+        "title": "제목1",
+        "description": "내용1",
+        "age_group": 5,
+        "limit": 5,
+        "tag": ["태그1", "태그2", "태그3"],
+        "image": "이미지1",
+        "member": null,
+        "member_cnt": 5
+    },
+    {
+        "id": 3,
+        "location": "지역1",
+        "field": "분야1",
+        "title": "제목1",
+        "description": "내용1",
+        "age_group": 5,
+        "limit": 5,
+        "tag": ["태그1", "태그2", "태그3"],
+        "image": "이미지1",
+        "member": null,
+        "member_cnt": 5
+    }, 
+    {
+        "id": 4,
+        "location": "지역1",
+        "field": "분야1",
+        "title": "제목1",
+        "description": "내용1",
+        "age_group": 5,
+        "limit": 5,
+        "tag": ["태그1", "태그2", "태그3"],
+        "image": "이미지1",
+        "member": null,
+        "member_cnt": 5
+    }
+];
+
+const club_data = [
+    {
+      'id': '1',
+      'leader_id': '12',
+      'name': '멋짱이 사자',
+      'location': '구미시 옥계동',
+      'member': '24',
+      'image': 'likelion.png'
+    },
+    {
+      'id': '2',
+      'leader_id': '24',
+      'name': '금오 산악회',
+      'location': '구미시 인동',
+      'member': '57',
+      'image': 'mountain.png'
+    },
+    {
+        'id': '3',
+        'leader_id': '24',
+        'name': '금오 산악회',
+        'location': '구미시 인동',
+        'member': '57',
+        'image': 'mountain.png'
+    },
+    {
+        'id': '4',
+        'leader_id': '24',
+        'name': '금오 산악회',
+        'location': '구미시 인동',
+        'member': '57',
+        'image': 'mountain.png'
+    }
+];
+
+const community_data = [
+    {
+        'writer_id': 1,
+        'title': '퇴사하고싶어요',
+        'date' : '2022'
+    }, 
+    {
+        'writer_id': 1,
+        'title': '퇴사하고싶어요',
+        'date' : '2022'
+    }, 
+    {
+        'writer_id': 1,
+        'title': '퇴사하고싶어요',
+        'date' : '2022'
+    }, 
+    {
+        'writer_id': 1,
+        'title': '퇴사하고싶어요',
+        'date' : '2022'
+    }, 
+    {
+        'writer_id': 1,
+        'title': '퇴사하고싶어요',
+        'date' : '2022'
+    }
+]
+
+export { lecture_data, mentoring_data, club_data, community_data}
