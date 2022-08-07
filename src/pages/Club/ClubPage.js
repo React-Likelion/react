820c8ce2178b51428a82cd7cfdcce837b808d86b
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-08-07 21:39:38 +0900

    fix css & typo

--- src/pages/Club/ClubPage.js
+++ src/pages/Club/ClubPage.js
@@ -4,7 +4,7 @@ import "../../style/pages/Club/ClubPage.css"
 const ClubPage = () => {
     const dummyData = {
             id : 1,
-            tag : [사업, 기획, 창업],
+            tag : ['사업', '기획', '창업'],
             title : "사업계획서 기획에서 창업 지원사업까지",
             content : "예비 창업자, 창업에 관심있는 분들 ~",
             writer : "모네",
