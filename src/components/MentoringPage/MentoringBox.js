e76d64e13329669c7bfe1019f77843c160d0d1f4
freemoon99 <soloist132@gmail.com>
2022-08-07 11:40:26 +0900

    배경색 수정

--- src/components/MentoringPage/MentoringBox.js
+++ src/components/MentoringPage/MentoringBox.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import MentoringItem from './MentoringItem';
 import '../../style/components/MentoringPage/MentoringBox.css'
-import { useNavigate } from 'react-router-dom';
 
 function MentoringBox() {
-    const navigate = useNavigate();
 
-    const onClickItemBox = () => {
-        navigate('/mentoring/detail')
-    }
+    
     return (
-        <div className='mentoringBox' onClick={onClickItemBox}>
+        <div className='mentoringBox' >
             {/* {
                 데이터명.map((element) => (
                     <MentoringItem props/>
                 ))
             } */}
+                <MentoringItem />
                 <MentoringItem />
                 <MentoringItem />
                 <MentoringItem />
