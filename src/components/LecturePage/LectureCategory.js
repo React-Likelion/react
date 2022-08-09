20671aa5c17c911f65f3c4dd203fff78d165b788
forever2969 <forever296@naver.com>
2022-08-10 02:27:48 +0900

    강의메인페이지/ 강의디테일페이지 일부기능 추가

--- src/components/LecturePage/LectureCategory.js
+++ src/components/LecturePage/LectureCategory.js
@@ -4,11 +4,13 @@ import '../../style/components/LecturePage/LectureCategory.css';
 const LectureCategory = ({setDetailCategoryData,setCategoryData,categoryData}) => {
     const [detailIt,setDetailIt] = useState(true);
     const [detailFinance,setDetailFinance] = useState(true);
-    const [detailProductPlan,setDetailProductPlan] = useState(true);
+    const [detailExercise,setDetailExercise] = useState(true);
     const [detailBusiness,setDetailBusiness] = useState(true);
     const [detailMarketing,setDetailMarketing] = useState(true);
-    const [detailDesign,setDetailDesign] = useState(true);
+    const [detailArt,setDetailArt] = useState(true);
     const [detailForeign,setDetailForeign] = useState(true);
+    const [detailCook,setDetailCook] = useState(true);
+    const [detailLifeStyle,setDetailLifeStyle] = useState(true);
     const ref = useRef();
     const infoArray = [
         'IT 프로그래밍',
@@ -16,13 +18,25 @@ const LectureCategory = ({setDetailCategoryData,setCategoryData,categoryData}) =
         '제품 기획',
         '비즈니스',
         '마케팅',
-        '디자인',
+        '미술',
         '외국어',
+        '요리',
+        '운동',
+        '라이프 스타일',
     ];
     
     const clickIt = (e)=>{
         // console.log((e.target.innerHTML).split('<')[0]);
         //console.log(ref.current.__reactProps$fn9mcx9ek2k.children[0]);
+        setDetailFinance(true);
+        setDetailExercise(true);
+        setDetailBusiness(true);
+        setDetailMarketing(true);
+        setDetailArt(true);
+        setDetailForeign(true);
+        setDetailCook(true);
+        setDetailLifeStyle(true);
+
         setDetailIt(!detailIt);
         const categoryDataBool = categoryData==='';
         console.log(categoryDataBool);
@@ -51,6 +65,14 @@ const LectureCategory = ({setDetailCategoryData,setCategoryData,categoryData}) =
         }
     };
     const clickFinance = (e)=>{
+        setDetailExercise(true);
+        setDetailBusiness(true);
+        setDetailMarketing(true);
+        setDetailArt(true);
+        setDetailForeign(true);
+        setDetailCook(true);
+        setDetailLifeStyle(true);
+        setDetailIt(true);
         setDetailFinance(!detailFinance);
         const categoryDataBool = categoryData==='';
         console.log(categoryDataBool);
@@ -77,8 +99,16 @@ const LectureCategory = ({setDetailCategoryData,setCategoryData,categoryData}) =
             }
         }
     };
-    const clickProductPlan = (e)=>{
-        setDetailProductPlan(!detailProductPlan);
+    const clickExercise = (e)=>{
+        setDetailFinance(true);
+        setDetailBusiness(true);
+        setDetailMarketing(true);
+        setDetailArt(true);
+        setDetailForeign(true);
+        setDetailCook(true);
+        setDetailLifeStyle(true);
+        setDetailIt(true);
+        setDetailExercise(!detailExercise);
         const categoryDataBool = categoryData==='';
         console.log(categoryDataBool);
         let existenceBool = false;
@@ -105,6 +135,14 @@ const LectureCategory = ({setDetailCategoryData,setCategoryData,categoryData}) =
         }
     };
     const clickBusiness = (e)=>{
+        setDetailFinance(true);
+        setDetailExercise(true);
+        setDetailMarketing(true);
+        setDetailArt(true);
+        setDetailForeign(true);
+        setDetailCook(true);
+        setDetailLifeStyle(true);
+        setDetailIt(true);
         setDetailBusiness(!detailBusiness);
         const categoryDataBool = categoryData==='';
         console.log(categoryDataBool);
@@ -132,6 +170,14 @@ const LectureCategory = ({setDetailCategoryData,setCategoryData,categoryData}) =
         }
     };
     const clickMarketing = (e)=>{
+        setDetailFinance(true);
+        setDetailExercise(true);
+        setDetailBusiness(true);
+        setDetailArt(true);
+        setDetailForeign(true);
+        setDetailCook(true);
+        setDetailLifeStyle(true);
+        setDetailIt(true);
         setDetailMarketing(!detailMarketing);
         const categoryDataBool = categoryData==='';
         console.log(categoryDataBool);
@@ -158,8 +204,16 @@ const LectureCategory = ({setDetailCategoryData,setCategoryData,categoryData}) =
             }
         }
     };
-    const clickDesign = (e)=>{
-        setDetailDesign(!detailDesign);
+    const clickArt = (e)=>{
+        setDetailFinance(true);
+        setDetailExercise(true);
+        setDetailBusiness(true);
+        setDetailMarketing(true);
+        setDetailForeign(true);
+        setDetailCook(true);
+        setDetailLifeStyle(true);
+        setDetailIt(true);
+        setDetailArt(!detailArt);
         const categoryDataBool = categoryData==='';
         console.log(categoryDataBool);
         let existenceBool = false;
@@ -186,6 +240,14 @@ const LectureCategory = ({setDetailCategoryData,setCategoryData,categoryData}) =
         }
     };
     const clickForeign = (e)=>{
+        setDetailFinance(true);
+        setDetailExercise(true);
+        setDetailBusiness(true);
+        setDetailMarketing(true);
+        setDetailArt(true);
+        setDetailCook(true);
+        setDetailLifeStyle(true);
+        setDetailIt(true);
         setDetailForeign(!detailForeign);
         const categoryDataBool = categoryData==='';
         console.log(categoryDataBool);
@@ -212,6 +274,76 @@ const LectureCategory = ({setDetailCategoryData,setCategoryData,categoryData}) =
             }
         }
     };
+    const clickCook = (e)=>{
+        setDetailFinance(true);
+        setDetailExercise(true);
+        setDetailBusiness(true);
+        setDetailMarketing(true);
+        setDetailArt(true);
+        setDetailForeign(true);
+        setDetailLifeStyle(true);
+        setDetailIt(true);
+        setDetailCook(!detailCook);
+        const categoryDataBool = categoryData==='';
+        console.log(categoryDataBool);
+        let existenceBool = false;
+        for(let value of infoArray){
+            if((e.target.innerHTML).split('<')[0]===value){
+                existenceBool=true;
+            }
+        }
+        if(categoryDataBool){
+            setCategoryData((e.target.innerHTML).split('<')[0]);
+        }else if(existenceBool){
+            for(let i=0; i<infoArray.length; i++){
+                if(infoArray[i]===(e.target.innerHTML).split('<')[0]){
+                    infoArray.splice(i,1);
+                    break;
+                }
+            }
+            for(let value of infoArray){
+                if(categoryData===value){
+                    setDetailCategoryData('');
+                    setCategoryData((e.target.innerHTML).split('<')[0]);
+                }
+            }
+        }
+    };
+    const clickLifeStyle = (e)=>{
+        setDetailFinance(true);
+        setDetailExercise(true);
+        setDetailBusiness(true);
+        setDetailMarketing(true);
+        setDetailArt(true);
+        setDetailForeign(true);
+        setDetailCook(true);
+        setDetailIt(true);
+        setDetailLifeStyle(!detailLifeStyle);
+        const categoryDataBool = categoryData==='';
+        console.log(categoryDataBool);
+        let existenceBool = false;
+        for(let value of infoArray){
+            if((e.target.innerHTML).split('<')[0]===value){
+                existenceBool=true;
+            }
+        }
+        if(categoryDataBool){
+            setCategoryData((e.target.innerHTML).split('<')[0]);
+        }else if(existenceBool){
+            for(let i=0; i<infoArray.length; i++){
+                if(infoArray[i]===(e.target.innerHTML).split('<')[0]){
+                    infoArray.splice(i,1);
+                    break;
+                }
+            }
+            for(let value of infoArray){
+                if(categoryData===value){
+                    setDetailCategoryData('');
+                    setCategoryData((e.target.innerHTML).split('<')[0]);
+                }
+            }
+        }
+    }
     const clickCategory = (e)=>{
         const lectureCategory = e.target.innerHTML;
         console.log(e);
@@ -233,11 +365,8 @@ const LectureCategory = ({setDetailCategoryData,setCategoryData,categoryData}) =
                     {
                         (detailIt) ||
                         <ul>
-                            <li onClick={clickCategory}>DevOps</li>
-                            <li onClick={clickCategory}>WEB</li>
-                            <li onClick={clickCategory}>Game</li>
-                            <li onClick={clickCategory}>Network</li>
-                            <li onClick={clickCategory}>Security</li>
+                            <li onClick={clickCategory}>웹/앱</li>
+                            <li onClick={clickCategory}>IT교양</li>
                         </ul>
                     }
                     </li>
@@ -251,22 +380,25 @@ const LectureCategory = ({setDetailCategoryData,setCategoryData,categoryData}) =
                         (detailFinance)||
                         <ul>
                             <li onClick={clickCategory}>주식</li>
-                            <li onClick={clickCategory}>예금</li>
-                            <li onClick={clickCategory}>적금</li>
+                            <li onClick={clickCategory}>제테크</li>
+                            <li onClick={clickCategory}>부동산</li>
+                            <li onClick={clickCategory}>창업</li>
+                            <li onClick={clickCategory}>부업</li>
                         </ul>
                     }
                         
                     </li>
                     <li
                     style={{cursor:'pointer'}}
-                    onClick={clickProductPlan}
+                    onClick={clickExercise}
                     ref={ref}
-                    >제품 기획
+                    >운동
                     {
-                        (detailProductPlan) ||
+                        (detailExercise) ||
                         <ul>
-                            <li onClick={clickCategory}>제품</li>
-                            <li onClick={clickCategory}>기획</li>
+                            <li onClick={clickCategory}>요가/필라테스</li>
+                            <li onClick={clickCategory}>헬스</li>
+                            <li onClick={clickCategory}>기타</li>
                         </ul>
                     }
                     </li>
@@ -301,11 +433,11 @@ const LectureCategory = ({setDetailCategoryData,setCategoryData,categoryData}) =
                     </li>
                     <li
                     style={{cursor:'pointer'}}
-                    onClick={clickDesign}
+                    onClick={clickArt}
                     ref={ref}
-                    >디자인
+                    >미술
                     {
-                        (detailDesign)||
+                        (detailArt)||
                         <ul>
                             <li onClick={clickCategory}>일러스트레이션</li>
                             <li onClick={clickCategory}>만화</li>
@@ -322,8 +454,36 @@ const LectureCategory = ({setDetailCategoryData,setCategoryData,categoryData}) =
                         (detailForeign)||
                         <ul>
                             <li onClick={clickCategory}>영어</li>
-                            <li onClick={clickCategory}>중국어</li>
-                            <li onClick={clickCategory}>일본어</li>
+                            <li onClick={clickCategory}>제2 외국어</li>
+                        </ul>
+                    }
+                    </li>
+                    <li
+                    style={{cursor:'pointer'}}
+                    onClick={clickCook}
+                    ref={ref}
+                    >요리
+                    {
+                        (detailCook)||
+                        <ul>
+                            <li onClick={clickCategory}>요리</li>
+                            <li onClick={clickCategory}>베이킹</li>
+                            <li onClick={clickCategory}>음료 및 주류</li>
+                        </ul>
+                    }
+                    </li>
+                    <li
+                    style={{cursor:'pointer'}}
+                    onClick={clickLifeStyle}
+                    ref={ref}
+                    >라이프 스타일
+                    {
+                        (detailLifeStyle)||
+                        <ul>
+                            <li onClick={clickCategory}>법률</li>
+                            <li onClick={clickCategory}>상담</li>
+                            <li onClick={clickCategory}>뷰티</li>
+                            <li onClick={clickCategory}>기타</li>
                         </ul>
                     }
                     </li>
--- src/pages/Lecture/LectureDetailPage.js
+++ src/pages/Lecture/LectureDetailPage.js
@@ -15,6 +15,8 @@ const LectureDetailPage = () => {
     const [purchaseStatus,setPurchaseStatus] = useState(false);
     const [myLecutureStatus,setMyLectureStatus] = useState(false);
     const [classModify,setClassModify] = useState(false);
+    const [likeState,setLikeState] = useState(false);
+    const [likeCount,setLikeCount] = useState(0);
 
     useEffect(()=>{
         //강의를 구매하였는지에 대한 state 실행
@@ -42,7 +44,22 @@ const LectureDetailPage = () => {
     const purchaseLecture = ()=>{
         setPurchaseStatus(!purchaseStatus);
     };
-
+    const clickUnFill = ()=>{
+        if(!purchaseStatus){
+            alert('먼저 강의를 구매하세요!');
+        }else{
+            setLikeState(!likeState);
+            setLikeCount(likeCount + 1);
+        }
+    };
+    const clickFill = ()=>{
+        if(!purchaseStatus){
+            alert('먼저 강의를 구매하세요!');
+        }else{
+            setLikeCount(likeCount - 1);
+            setLikeState(!likeState);
+        }
+    };
 
     return (
         <div>
@@ -53,13 +70,28 @@ const LectureDetailPage = () => {
                     <LectureLeft lectureImg={lectureImg} classModify={classModify} lectureTitle={lectureTitle} />
                 </section>
                 <section id="LectureRightSection">
-
+                    
                     {/*클래스 수정/시청/신청 잘 돌아가는지 확인용, 통신하고 구현하면 지울것*/}
                     --로직 확인용
                     <button type="button" onClick={myLecture}>나의 강의</button>
                     <button type="button" onClick={purchaseLecture}>구매한 강의</button> 로직 확인용--
 
                     <LectureRight lectureId={lectureId} lectureTitle={lectureTitle} lecturePrice={lecturePrice} />
+                    {/* 조건
+                    1. 로그아웃 상태일때 , 하트는 빈하트, 좋아요 수는 표시해준다.
+                    2. 로그인 상태일때,
+                      1) 구매를 했다면, 하트를 누를 수 있도록 한다.
+                      2) 구매를 하지 않았다면, 하트를 못누르도록 하고 빈하트로만 표시한다.
+                      3) 나의 강의라면?
+                        1- 구매를 하지 않은 상태처럼 표시한다. (자기거에 좋아요 누를 수 없음) */}
+                    <article id="likeArticle">
+                    {
+                        (likeState)?
+                        <img onClick={clickFill} src='/img/fillHeart.png' alt='하트' />:
+                        <img onClick={clickUnFill} src='/img/unfillHeart.png' alt='빈하트' />
+                    }
+                    <span>{likeCount}</span>
+                    </article>
                     {
                         (myLecutureStatus)?
                         <button type="button" onClick={clickLectureModify}>클래스 수정하기</button>:
@@ -67,6 +99,8 @@ const LectureDetailPage = () => {
                         <button type="button" onClick={clickWatch}>클래스 시청하기</button>:
                         <button type="button" onClick={clickRegistration}>클래스 신청하기</button>
                     }
+
+                    
                 </section>
             </div>
         </div>
