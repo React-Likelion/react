f21dc31fc7b77e06538132f2ce61da03cf762e17
JaeHyunGround <gothddlek@naver.com>
2022-08-10 23:25:25 +0900

    Filter finish

--- src/components/Filter.js
+++ src/components/Filter.js
@@ -1,5 +1,6 @@
-import React, {useState} from "react";
-import "../style/components/Filter.css"
+import React, {useState, useEffect} from "react";
+import "../style/components/Filter.css";
+import {PROXY} from "../data/serverUrl";
 
 const locationList = [
     "전체",
@@ -51,57 +52,106 @@ const personnelList = [
 ]
 
 const Filter = () => {
-    const [itemToggle, setItemToggle] = useState(false);
+    const [choiceitem, setChoiceItem] = useState("");
+    const [itemList, setItemList] = useState([]);
+    const [url, setUrl] = useState(`${PROXY}/?`);
     // 통신할 땐 배열에 담긴 요소들을 문자열로 바꿔서(toString 함수 사용) url에 쿼리 스트링으로 넣어주면 될 듯
-    const [choiceLocationList, setChoiceLocationList] = useState([]);
+    
+    const [choicedLocationList, setChoiceLocationList] = useState([]);
     const [choicedFieldList, setChoicedFieldList] = useState([]);
     const [choicedAgeList, setChoicedAgeList] = useState([]);
     const [choicedPersonnelList, setChoicedPersonnelList] = useState([]);
 
     const choicedLocation = (e) => {
         // 선택한 옵션에 내가 누른 옵션이 포함되어 있지 않을 때
-        if(!choiceLocationList.includes(e.target.id)) {
-            setChoiceLocationList([...choiceLocationList, e.target.id])
+        if(!choicedLocationList.includes(e.target.id)) {
+            setChoiceLocationList([...choicedLocationList, e.target.id])
+            setUrl(url + `location=${encodeURIComponent(e.target.id)}&`)
         }
         // 선택한 옵션에 내가 누른 옵션이 포함되어 있을 때
-         else {
-            const newChoiceLocationList = choiceLocationList.filter((item) => item !== e.target.id)
+         else {  
+            const newChoiceLocationList = choicedLocationList.filter((item) => item !== e.target.id)
             setChoiceLocationList(newChoiceLocationList);
+            setUrl(url.replace(`location=${encodeURIComponent(e.target.id)}&`, ""))
+        }
+        if(!itemList.includes(e.target.id)) {
+            setChoiceItem(e.target.id);
+            setItemList([...itemList, e.target.id]);
+        }
+        else {
+            const newItemList = itemList.fillter((it) => it !== e.target.id)
+            setItemList(newItemList)
         }
     }
     const choicedField = (e) => {
         // 선택한 옵션에 내가 누른 옵션이 포함되어 있지 않을 때
         if(!choicedFieldList.includes(e.target.id)) {
             setChoicedFieldList([...choicedFieldList, e.target.id])
+            setUrl(url + `field=${encodeURIComponent(e.target.id)}&`)
         }
         // 선택한 옵션에 내가 누른 옵션이 포함되어 있을 때 
         else {
             const newChoicedFieldList = choicedFieldList.filter((item) => item !== e.target.id)
             setChoicedFieldList(newChoicedFieldList);
+            setUrl(url.replace(`field=${encodeURIComponent(e.target.id)}&`, ""))
+        }
+        if(!itemList.includes(e.target.id)) {
+            setChoiceItem(e.target.id);
+            setItemList([...itemList, e.target.id]);
+        }
+        else {
+            const newItemList = itemList.fillter((it) => it !== e.target.id)
+            setItemList(newItemList)
         }
     }
     const choicedAge = (e) => {
         // 선택한 옵션에 내가 누른 옵션이 포함되어 있지 않을 때
         if(!choicedAgeList.includes(e.target.id)) {
             setChoicedAgeList([...choicedAgeList, e.target.id])
+            setUrl(url + `age=${encodeURIComponent(e.target.id)}&`)
         }
         // 선택한 옵션에 내가 누른 옵션이 포함되어 있을 때 
         else {
             const newChoicedAgeList = choicedAgeList.filter((item) => item !== e.target.id)
             setChoicedAgeList(newChoicedAgeList);
+            setUrl(url.replace(`age=${encodeURIComponent(e.target.id)}&`, ""))
+        }
+        if(!itemList.includes(e.target.id)) {
+            setChoiceItem(e.target.id);
+            setItemList([...itemList, e.target.id]);
+        }
+        else {
+            const newItemList = itemList.fillter((it) => it !== e.target.id)
+            setItemList(newItemList)
         }
     }
     const choicedPersonnel = (e) => {
         // 선택한 옵션에 내가 누른 옵션이 포함되어 있지 않을 때
         if(!choicedPersonnelList.includes(e.target.id)) {
             setChoicedPersonnelList([...choicedPersonnelList, e.target.id])
+            setUrl(url + `limit=${encodeURIComponent(e.target.id)}&`)
         }
         // 선택한 옵션에 내가 누른 옵션이 포함되어 있을 때
         else {
             const newChoicedPersonnelList = choicedPersonnelList.filter((item) => item !== e.target.id)
             setChoicedPersonnelList(newChoicedPersonnelList);
+            setUrl(url.replace(`limit=${encodeURIComponent(e.target.id)}&`, ""))
+        }
+        if(!itemList.includes(e.target.id)) {
+            setChoiceItem(e.target.id);
+            setItemList([...itemList, e.target.id]);
+        }
+        else {
+            const newItemList = itemList.fillter((it) => it !== e.target.id)
+            setItemList(newItemList)
         }
     }
+
+    useEffect(() => {
+        // axios 통신
+        let axiosUrl = url.slice(0, url.length - 1);
+        console.log(axiosUrl)
+    }, [url])
 
     return (
         <section>
@@ -110,7 +160,7 @@ const Filter = () => {
                     <p className="FilterTitle">지역</p>
                     <div className="FilterItems">
                         {
-                        locationList.map((item) => <span onClick={choicedLocation} id={item} className={itemToggle ? "FilterItem-choice" : "FilterItem"}>{item}</span>)
+                        locationList.map((item) => <span onClick={choicedLocation} id={item} className={"FilterItem" + (itemList.includes(item) ? "-choice" : "")}>{item}</span>)
                         }
                     </div>
                 </div>
@@ -118,7 +168,7 @@ const Filter = () => {
                     <p className="FilterTitle">분야</p>
                     <div className="FilterItems">
                         {
-                        FieldList.map((item) => <span onClick={choicedField} id={item} className={itemToggle ? "FilterItem-choice" : "FilterItem"}>{item}</span>)
+                        FieldList.map((item) => <span onClick={choicedField} id={item} className={"FilterItem" + (itemList.includes(item) ? "-choice" : "")}>{item}</span>)
                         }
                     </div>
                 </div>
@@ -126,7 +176,7 @@ const Filter = () => {
                     <p className="FilterTitle">연령</p>
                     <div className="FilterItems">
                         {
-                        ageList.map((item) => <span onClick={choicedAge} id={item} className={itemToggle ? "FilterItem-choice" : "FilterItem"}>{item}</span>)
+                        ageList.map((item) => <span onClick={choicedAge} id={item} className={"FilterItem" + (itemList.includes(item) ? "-choice" : "")}>{item}</span>)
                         }
                     </div>
                 </div>
@@ -134,7 +184,7 @@ const Filter = () => {
                     <p className="FilterTitle">인원수</p>
                     <div className="FilterItems">
                         {
-                        personnelList.map((item) => <span onClick={choicedPersonnel} id={item} className={itemToggle ? "FilterItem-choice" : "FilterItem"}>{item}</span>)
+                        personnelList.map((item) => <span onClick={choicedPersonnel} id={item} className={"FilterItem" + (itemList.includes(item) ? "-choice" : "")}>{item}</span>)
                         }
                     </div>
                 </div>
