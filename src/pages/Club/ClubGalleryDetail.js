b50ee9ff97a88398bbf706b64cad1d7a0373e980
JaeHyunGround <gothddlek@naver.com>
2022-08-11 21:08:00 +0900

    clubGalleryDetail style

--- src/pages/Club/ClubGalleryDetail.js
+++ src/pages/Club/ClubGalleryDetail.js
@@ -1,9 +1,41 @@
-import React from "react";
+import React, {useState} from "react";
+import Header from "../../components/Header";
+import Navbar from '../../components/Navbar';
+import "../../style/pages/Club/ClubGalleryDetail.css"
+import Modal from 'react-bootstrap/Modal';
 
 const ClubGalleryDetail = () => {
+    const [clubGalleryDetailModal, setClubGalleryDetailModal] = useState(false);
+
+    const modalHandle = () => {
+        setClubGalleryDetailModal(!clubGalleryDetailModal);
+    }
+    const clubGalleryDetailhandleClose = () => {
+        setClubGalleryDetailModal(false)
+    }
+
     return (
-        <setion>
-            ClubGalleryDetail        
+        <setion className="ClubGalleryDetail">
+            <Header/>
+            <Navbar/>
+            <div className="clubGalleryDetailContainer">
+                <div className="clubGalleryDetailBox">
+                    <img onClick={modalHandle} className='clubGalleryDetailImg' src={`${process.env.PUBLIC_URL}/img/Example.png`}/>
+                    <p className="clubGalleryDetailTitlle">오늘 저녁은 고기를 먹었따.</p>
+                </div>
+            </div>
+
+            {
+                (!clubGalleryDetailModal) || <div id="modalDiv">
+                        <Modal className="modal-container" show={clubGalleryDetailModal} onHide={clubGalleryDetailhandleClose}>
+                            <img src={`${process.env.PUBLIC_URL}/img/Example.png`} alt="클럽 디테일 이미지"/><br/>
+                            <p>&nbsp;오늘 저녁은 고기를 먹었따.</p>
+                            <hr/>
+                            <button id="clubGalleryDetailBtn" type="button" onClick={clubGalleryDetailhandleClose}>닫기</button>
+                        </Modal>
+                    </div>
+            }
+
         </setion>
     );
 }
