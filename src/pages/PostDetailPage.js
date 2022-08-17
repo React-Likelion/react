4b8de3db902c1f23d531c8b627be1946a3396c17
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-08-17 11:08:28 +0900

    add post crud axios

--- src/pages/PostDetailPage.js
+++ src/pages/PostDetailPage.js
@@ -1,13 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import '../style/pages/PostDetailPage.css';
 import Header from '../components/Header.js';
 import Navbar from '../components/Navbar';
 import DetailRepleBox from '../components/CommunityPage/DetailRepleBox';
+import axios from 'axios';
+import { PROXY } from '../data/serverUrl';
+import { useNavigate } from 'react-router-dom';
 
 const PostDetailPage = () => {
 
     const {id} = useParams();
+    const navigate = useNavigate();
+    const [detailInfo, setDetailInfo] = useState({});
+
+    // 게시물 수정
+    const clickModify = (e) => {
+        if(window.confirm('해당 글을 수정하시겠습니까 ?')) {
+            // 수정
+        }
+    }
+
+    // 게시물 삭제
+    const clickDelete = (e) => {
+        if(window.confirm('해당 글을 삭제하시겠습니까 ?')) {
+            axios.delete(`${PROXY}/community/${id}/`, {
+                headers: {
+                    'Authorization': 'Bearer '+localStorage.getItem('react_accessToken')
+                  }
+              })
+            .then((res) => {
+                console.log(res);
+                navigate('/community');
+            })
+            .catch((err) => {
+                console.log(err);
+            })
+        }
+    }
+
+    // 게시물 조회
+    useEffect(() => {
+        axios.get(`${PROXY}/community/${id}/`)
+        .then((res) => {
+            setDetailInfo(res.data);
+        })
+        .catch((err) => {
+            console.log(err);
+        })
+    }, []);
 
     return (
         <section className='PostDetailPage'>
@@ -15,16 +56,21 @@ const PostDetailPage = () => {
             <Navbar/>
             <div className='post-detail-container'>
                 <div id='post-detail-content'>
-                    <div>카테고리</div>
+                    <div>{detailInfo.category}</div>
                     <div id='post-detail-writer-box'>
                         <img src='' alt='x'/>
-                        <div>이름 <br/>08/13 04:56</div>
+                        <div>{detailInfo.writer_id} <br/>{detailInfo.create_time && detailInfo.create_time.substr(0, 10)}</div>
+                        {detailInfo.writer_id === localStorage.getItem('react_nickname') &&
+                            <div id='detail-btns'>
+                                <button onClick={clickModify}>수정</button>
+                                <button onClick={clickDelete}>삭제</button>
+                            </div>}
                     </div>
-                    <div>다들 반가워요 ~~~</div>
-                    <div>오늘 퇴직하고 바로 달려와서 가입합니다. <br/>가끔 도망치고 싶었는데, 오늘이 바로 그 날인 듯합니다.</div>
-                    <br/><img src='' alt='x'/><br/><br/>
+                    <div>{detailInfo.title}</div>
+                    <div>{detailInfo.description}</div>
+                    <br/><img src={detailInfo.image} alt='x'/><br/><br/>
                 </div>
-                <DetailRepleBox/>
+                <DetailRepleBox post_id={id}/>
             </div>
         </section>
     );
