09a02bfba7f43478c55740b677c858f6acefe740
freemoon99 <soloist132@gmail.com>
2022-09-21 10:58:14 +0900

    멘토링페이지 수정

--- src/components/MentoringPage/MentoringBox.js
+++ src/components/MentoringPage/MentoringBox.js
@@ -1,7 +1,6 @@
-import React, {useState, useEffect} from 'react';
+import React from 'react';
 import MentoringItem from './MentoringItem';
 import '../../style/components/MentoringPage/MentoringBox.css'
-import axios from "axios";
 // import { PROXY } from '../../data/serverUrl';
 
 function MentoringBox({datas}) {
